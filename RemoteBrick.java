d0660a9efe63734afecfa3bd6f470825389eedc6
Eirik Hemstad <eirikzh@gmail.com>
2018-11-09 16:00:48 +0100

    Added changes for implementing gyroscope and playing sounds

--- RemoteBrick.java
+++ RemoteBrick.java
@@ -1,9 +1,25 @@
+/*
+Denne klassen lagde vi og den brukes for å koble til og styre motorer på en annen EV3.  Men før denne vil funke må det gjøres noen greier.
+1) Passe på at de har ulike navn i instillingene. Vi kaller våre EV2, EV3 OG EV4.
+2) Parre dem gjennom bluetooth (med de nye navnene)
+3) Gå i den tingen som heter PAN og sett den ene som host og de andre som bluetooth-et eller annet.
+Den som er host er den dere burde laste opp koden til.
+
+I koden styrer dere en motor slik:
+    RemoteBrick ev2 = new RemoteBrick;
+    ev2.setSpeedAll(900);
+    ev2.forward("A");
+    ev2.closeAll()          //OBS OBS, denne er ganske viktig, for motorene på remote-ev3 stopper ikke når koden stopper, men når de blir bedt om det.
+*/
 import lejos.hardware.Brick;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.motor.*;
 import lejos.robotics.*;
 import lejos.remote.ev3.*;
-
+import lejos.hardware.Sound;
+import lejos.hardware.Audio;
+import java.io.File;
+import java.util.Random;
 
 public class RemoteBrick {
     public String remoteName;
@@ -11,20 +27,32 @@ public class RemoteBrick {
     public RemoteRequestEV3 brick;
     public int motorCount;
     public String[] bokstaver = {"A","B","C","D"};
+    //lyd
+    public Audio audio;
+    public File[] files = new File[5];
+	public Random random = new Random();
 
     public RemoteBrick(String name, int motorCount){
         this.motorCount = motorCount;
         this.remoteName = name;
 
+        //kobler til Remote EV3
         try {
-            System.out.println("Connecting to " + remoteName);
+            System.out.println("Connecting to "+remoteName);
             this.brick = new RemoteRequestEV3(BrickFinder.find(remoteName)[0].getIPAddress());
-            //Motorer
-            System.out.println("Initializing motors at " + remoteName);
+            //Initialiserer motorer og legger dem til i en array: motors.
+            System.out.println("Initializing motors at "+remoteName);
             this.motors = new RegulatedMotor[motorCount];
             for(int i=0;i<motorCount;i++){
                 this.motors[i] = brick.createRegulatedMotor(bokstaver[i], 'L');
             }
+            //lyd
+            audio = this.brick.getAudio();
+            audio.setVolume(100);
+            for(int i = 0;i<files.length;i++){
+    			String fileName = "moo"+i+".wav";
+    			files[i] = new File(fileName);
+    		}
         }
 
         catch (Exception e){
@@ -128,20 +156,34 @@ public class RemoteBrick {
             }
         }
     }
-
+    //Sier til remote EV3 at man er ferdig med å bruke motorene.
     public void closeAll(){
         System.out.println("Closing motors at " + remoteName);
         for(int i = 0; i < motorCount; i++){
             motors[i].close();
         }
     }
+    public void moo() {
+		int i = random.nextInt(5);
+		System.out.println("Spiller lyd: "+files[i]);
+		//duration er tiden det tar å spille av lyden
+		int dur = Sound.playSample(files[i]);
+		if(dur<0){
+			System.out.println("En feil har skjedd ved avspilling av lyd: "+files[i]);
+		}
+		try {
+			Thread.sleep(dur+10);
+		}
+		catch(Exception e){
 
+		}
+	}
     public void remoteMotorTest(){
         System.out.println("Setting speed.");
         for(int i=0;i<motors.length;i++){
             motors[i].setSpeed(900);
         }
-        System.out.println("Rotating.");
+        System.out.println("Forward.");
         for(int i=0;i<motors.length;i++){
             motors[i].forward();
         }
--- RobotKu.java
+++ RobotKu.java
@@ -3,9 +3,9 @@
  * Authors:         Cato Bakken, Eirik Hemstad, Jonas B. Jacobsen, Torbjørn B. Lauvvik, Torstein H. Sundfær
 */
 
+import lejos.hardware.*;
 import lejos.hardware.motor.*;
 import lejos.hardware.lcd.*;
-import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.hardware.port.*;
 import lejos.hardware.Brick;
 import lejos.hardware.BrickFinder;
@@ -21,9 +21,13 @@ public class RobotKu {
     // Initialize EV3 objects
     private static Brick brick;
     private static TextLCD lcd;
-    private static Port p1, p2, p3;             // p1: touchSensor | p2: forward ultrasonic | p3: downward ultrasonic
+    private static Port p1;
     private static SampleProvider touchSensor;
     private static float[] touchSample;
+    private static SampleProvider angle;
+    private static float[] gyroSample;
+    public static EV3GyroSensor gyroSensor;
+
 
     //remote ev3
     private static RemoteBrick ev3;
@@ -46,7 +50,8 @@ public class RobotKu {
         setupEV3();     // Setup EV3 components
 
         standUp();
-
+        //playMoo();
+        //playMoo();
         // Start programrutine
 
         /*
@@ -59,13 +64,17 @@ public class RobotKu {
         }
         */
 
-        System.out.println("Waiting 10 seconds");
+        delay(1000);
+        hodeTest();
+        //playMoo();
+        System.out.println("Waiting 4 seconds");
+        calibrate();
 
-        delay(10000);
+        delay(4000);
 
         lieDown();
 
-        delay(8000);
+        delay(3000);
 
         // Slutt programrutine
 
@@ -80,7 +89,7 @@ public class RobotKu {
 
         System.out.println("Connecting to remote EV3 bricks.");
         try {
-            ev3 = new RemoteBrick("EV3", 4);        //Andre parameter er anntall motorer som skal settes opp.
+            ev3 = new RemoteBrick("EV3", 4); //bakbein       //Andre parameter er anntall motorer som skal settes opp.
             ev2 = new RemoteBrick("EV2", 4);
 
             ev3.setSpeedAll(LEG_SPEED);
@@ -98,23 +107,52 @@ public class RobotKu {
 
         try {
             p1 = brick.getPort("S1");
-            p2 = brick.getPort("S2");
-            p3 = brick.getPort("S3");
-            touchSensor = new EV3TouchSensor(p1);
+            //p2 = brick.getPort("S2");
+            //p3 = brick.getPort("S3");
+            //touchSensor = new EV3TouchSensor(p1);
+
+            //gyro
+    		gyroSensor = new EV3GyroSensor(p1);
+            angle = gyroSensor.getAngleMode();
+    		gyroSensor.reset();
         }
         catch (Exception ex) {
             System.out.println("Could not connect to sensor ports. Exception: " + ex);
         }
-        
+
         System.out.println("Setup complete.");
     }
 
+    private static void playMoo(){
+        ev2.moo();
+    }
+    private static void hodeTest(){
+        Motor.A.setSpeed(300);
+        Motor.B.setSpeed(50);
+        Motor.B.rotate(100);
+        while(Motor.B.isMoving()){
+            //wait
+        }
+        Motor.B.rotate(-100);
+        while(Motor.B.isMoving()){
+
+        }
+        for(int i = 0;i<15;i++){
+            Motor.A.rotate(60);
+            while(Motor.A.isMoving()){
+                //wait
+            }
+            Motor.A.rotate(-60);
+            while(Motor.A.isMoving()){
+                //wait
+            }
+        }
+    }
     private static void shutOff(){
         System.out.println("Adieu");
         ev2.closeAll();
         ev3.closeAll();
     }
-
     private static void startWalk(RemoteBrick rBrick, String side) {
         /*
         1: Rotate knee backwards
@@ -122,26 +160,28 @@ public class RobotKu {
         3: Rotate knee forward
         4: Rotate hip downwards
         */
-
         String hip;
         String knee;
 
         if (side.equals("L")) {
             hip = "A";
             knee = "B";
-        } else if (side.equals("R")) {
+        }
+        else if (side.equals("R")) {
             hip = "C";
             knee = "D";
-        } else {
+        }
+        else {
             throw new IllegalArgumentException("Invalid side. Please use either 'R' or 'L'.");
         }
-		
+
 		rBrick.setSpeedAll(10);
-		
-        rBrick.rotateTo(knee, 0);
-        delay(2000);
+
         rBrick.rotateTo(hip, 40);
         delay(1000);
+        rBrick.rotateTo(knee, 0);
+        delay(2000);
+
 
         rBrick.rotateTo(knee, 100);
         rBrick.rotateTo(hip, 10);
@@ -153,7 +193,7 @@ public class RobotKu {
 
     }
 
-    
+
 
     private static void delay(int duration){
         try {
@@ -163,10 +203,10 @@ public class RobotKu {
 			System.out.println("Got exception: " + e);
 		}
     }
-   
+
     private static void standUp() {
         System.out.println("Starting standing procedure.");
-		
+
 		int hipRotationPoint = 10;
 		int kneeRotationPoint = 100;
 
@@ -177,17 +217,54 @@ public class RobotKu {
         ev2.rotateTo("A", hipRotationPoint);
         ev3.rotateTo("C", hipRotationPoint);
         ev3.rotateTo("A", hipRotationPoint);
-        
+
         ev2.rotateTo("D", kneeRotationPoint);
         ev2.rotateTo("B", kneeRotationPoint);
         ev3.rotateTo("D", kneeRotationPoint);
         ev3.rotateTo("B", kneeRotationPoint);
 
-        delay(8000);
-
+        delay(50);
+        calibrate();
         System.out.println("Standing procedure completed");
     }
+    private static void calibrate(){
+        float currAngle = getAngle();
+        int CAL_SPEED = 10;
+        /*
+            TING SOM MÅ TETSTES HER
+            1) ER BACKWARD RIKTIG RETNING?
+            2) FUNKER DET?
+            3) FART? JUSTER
+            4) ALLE LEDD ELLER BARE HOFTE/KNE
+        */
+        //ev3 er foran ev3 er bak
+        if(currAngle>0){
+            System.out.println("Calibrating. Lowering back.");
+            //øk forbein eller senk bakbein
+            ev3.setSpeedAll(CAL_SPEED);
+            ev3.backward(MOTORS_ALL);
+            while(currAngle>0){
+                currAngle = getAngle();
+            }
+            ev3.stop(MOTORS_ALL);
 
+        }
+        else if(currAngle<0){
+            System.out.println("Calibrating. Lowering front.");
+            //øk bakbein eller senk forbein
+            ev2.setSpeedAll(CAL_SPEED);
+            ev2.backward(MOTORS_ALL);
+            while(currAngle<0){
+                currAngle = getAngle();
+            }
+            ev2.stop(MOTORS_ALL);
+        }
+    }
+    private static float getAngle(){
+		gyroSample = new float[angle.sampleSize()];
+		angle.fetchSample(gyroSample, 0);
+		return gyroSample[0];
+    }
     private static void lieDown() {
         System.out.println("Starting liedown procedure.");
 
