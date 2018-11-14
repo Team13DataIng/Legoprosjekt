0bf2de6fed5ab6d94e12e891b25921d9776b5c9b
Jonas BJ <jonas4a@gmail.com>
2018-11-14 13:03:29 +0100

    endre hhva vi importerer

--- RobotKu.java
+++ RobotKu.java
@@ -3,18 +3,18 @@
  * Authors:         Cato Bakken, Eirik Hemstad, Jonas B. Jacobsen, Torbjørn B. Lauvvik, Torstein H. Sundfær
 */
 
-import lejos.hardware.*;
-import lejos.hardware.motor.*;
-import lejos.hardware.lcd.*;
-import lejos.hardware.port.*;
+import lejos.hardware.motor.Motor;
+import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.port.Port;
 import lejos.hardware.Brick;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.ev3.EV3;
-import lejos.hardware.Keys;
 import lejos.robotics.SampleProvider;
-import lejos.hardware.sensor.*;
-import lejos.hardware.Button;
-import java.util.*;
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.EV3TouchSensor;
+
+
 
 public class RobotKu {
 
