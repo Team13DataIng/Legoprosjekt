50501ee046c2f8fa8bba98cb2ac2d7a3bbbf3893
Jonas BJ <jonas4a@gmail.com>
2018-11-14 13:38:29 +0100

    fjernet en import

--- RobotKu.java
+++ RobotKu.java
@@ -8,7 +8,6 @@ import lejos.hardware.lcd.TextLCD;
 import lejos.hardware.port.Port;
 import lejos.hardware.Brick;
 import lejos.hardware.BrickFinder;
-import lejos.hardware.ev3.EV3;
 import lejos.robotics.SampleProvider;
 import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
